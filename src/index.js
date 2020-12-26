e5f3fefe07ac8b3487065abf0ee54744d97e8ad2
lfuleki0 <lfuleki@yahoo.com>
2020-12-26 22:05:57 +0100

    update2 sd

--- src/index.js
+++ src/index.js
@@ -16,24 +16,20 @@ var users = defaultUsers.slice();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // setup a new database using database credentials set in .env
-var sequelize = new Sequelize(
-  "database",
-  process.env.USER,
-  process.env.PASSWORD,
-  {
-    host: "0.0.0.0",
-    dialect: "sqlite",
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
-    // Data is stored in the file `database.sqlite` in the folder `db`.
-    // Note that if you leave your app public, this database file will be copied if
-    // someone forks your app. So don't use it to store sensitive information.
-    storage: "/sandbox/src/db/database.sqlite"
-  }
-);
+var sequelize = new Sequelize("database", "Laszlo", "mylaszlosecret", {
+  host: "0.0.0.0",
+  dialect: "sqlite",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  // Data is stored in the file `database.sqlite` in the folder `db`.
+  // Note that if you leave your app public, this database file will be copied if
+  // someone forks your app. So don't use it to store sensitive information.
+  storage: "/sandbox/src/db/database.sqlite"
+});
+sequeliz;
 
 // authenticate with the database
 sequelize
